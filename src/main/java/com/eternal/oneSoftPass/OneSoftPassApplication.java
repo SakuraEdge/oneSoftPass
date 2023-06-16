208c527592aff1156f5b350e79585b0cc5a4ae03
xielj5 <xielj5@asiainfo.com>
2023-06-16 17:54:39 +0800

    初始框架

---
+++ src/main/java/com/eternal/oneSoftPass/OneSoftPassApplication.java
@@ -0,0 +1,13 @@
+package com.eternal.oneSoftPass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneSoftPassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneSoftPassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eternal/oneSoftPass/OneSoftPassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eternal.oneSoftPass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneSoftPassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
